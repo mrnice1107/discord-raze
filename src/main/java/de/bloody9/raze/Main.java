5fbee6260c12d6f826dedb3a8337efeb3dda6737
mrnice1107 <55033883+mrnice1107@users.noreply.github.com>
2020-11-10 18:23:01 +0100

    initial commit
    + .gitignore
    + pom.xml
    + main bot structure
    + implements rolesectionfeature

---
+++ src/main/java/de/bloody9/raze/Main.java
@@ -0,0 +1,7 @@
+package de.bloody9.raze;
+
+public class Main {
+    public static void main(String[] args) {
+        new Raze(args);
+    }
+}
---
+++ src/main/java/de/bloody9/raze/Raze.java
@@ -0,0 +1,30 @@
+package de.bloody9.raze;
+
+import de.bloody9.core.Bot;
+import de.bloody9.core.models.objects.BotInitObject;
+import de.bloody9.feature.rolesection.RoleSectionFeature;
+
+public class Raze extends Bot {
+
+    public static Raze INSTANCE;
+
+    public Raze(String[] args) {
+        this(enterArgs(args));
+    }
+
+    public Raze(BotInitObject initObject) {
+        super(initObject);
+    }
+
+    @Override
+    public void preInit(BotInitObject initObject) {
+        super.preInit(initObject);
+        INSTANCE = this;
+    }
+
+    @Override
+    public void addFeatures() {
+        super.addFeatures();
+        features.add(new RoleSectionFeature());
+    }
+}
